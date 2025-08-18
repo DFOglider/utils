48c7133da839f4e163af5af7e1bc5315d57fa6d5
Chantelle Layton <layton.chantelle@gmail.com>
2025-08-18 14:33:39 -0300

    add lon lat gps to timeseries

--- processingScripts/process_SeaExplorer_delayed.py
+++ processingScripts/process_SeaExplorer_delayed.py
@@ -2,8 +2,75 @@ import logging
 import os
 import netCDF4 as nc
 import re
+import pandas as pd
+import numpy as np
 import pyglider.seaexplorer as seaexplorer
 import pyglider.ncprocess as ncprocess
+# function to add [longitude, latitude]_gps and linearly interpolate between gps hits
+# to timeseries file
+def addGPS(inname):
+    src = nc.Dataset(inname, mode='a')
+    variables = list(src.variables.keys())
+    if 'DeadReckoning' in variables:
+        # create LATITUDE_GPS and LONGITUDE_GPS
+        ## this will be only coordinates where DeadReckoning == 0 and NavState == 116
+        ## order of states :
+        ### 119
+        ### 116 - transmitting
+        ### 110 - inflecting down -> 100 going down
+        ### 118 - inflecting up -> 117 going up
+        ### 115 - surfacing
+        ### 119
+        ### 116 - transmitting
+        dr = src.variables.get('DeadReckoning')[:]
+        ns = src.variables.get('NavState')[:]
+        ok = (dr == 0) & ~dr.mask & (ns == 116) & ~ns.mask
+    else: # use longitude and latitude values with navstate
+        ns = src.variables.get('NavState')[:]
+        ok = (ns == 116) & ~ns.mask
+    lon = src.variables.get('longitude')[:]
+    lat = src.variables.get('latitude')[:]
+    longps = pd.Series(np.ma.masked_array(data=lon.data, mask=ok.mask, fill_value=np.nan).filled())
+    longps.interpolate(method='linear', inplace=True)
+    latgps = pd.Series(np.ma.masked_array(data=lat.data, mask=ok.mask, fill_value=np.nan).filled())
+    latgps.interpolate(method='linear', inplace=True)
+    # add to file
+    ## get dimensions from existing variable
+    dimensions = src.variables.get('NavState').dimensions
+    ## create LONGITUDE_GPS
+    nclongps = src.createVariable(varname = "LONGITUDE_GPS",
+                                  datatype='f4',
+                                  dimensions=dimensions)
+    nclongps[:] = longps
+    ### add attributes
+    nclongps.coordinate_reference_frame='urn: ogc:crs: EPSG::4326'
+    nclongps.long_name = "Longitude east by unspecified GPS system"
+    nclongps.observation_type = "measured and interpolated"
+    nclongps.reference= "WGS84"
+    nclongps.standard_name = "longitude"
+    nclongps.units= "degree_east"
+    nclongps.unitsVocabulary= "http://vocab.nerc.ac.uk/collection/P06/current/DEGE/"
+    nclongps.valid_max= 180.00
+    nclongps.valid_min= -180.00
+    nclongps.vocabulary= "https://vocab.nerc.ac.uk/collection/OG1/current/LON_GPS/"
+        ## create LATITUDE_GPS
+    nclatgps = src.createVariable(varname="LATITUDE_GPS",
+                                  datatype='f4',
+                                  dimensions=dimensions)
+    nclatgps[:] = latgps
+    ### add attributes
+    nclatgps.coordinate_reference_frame='urn: ogc:crs: EPSG::4326'
+    nclatgps.long_name = "Latitude north by unspecified GPS system"
+    nclatgps.observation_type = "measured and interpolated"
+    nclatgps.reference= "WGS84"
+    nclatgps.standard_name = "latitude"
+    nclatgps.units= "degree_north"
+    nclatgps.unitsVocabulary= "http://vocab.nerc.ac.uk/collection/P06/current/DEGN/"
+    nclatgps.valid_max= 90.00
+    nclatgps.valid_min= -90.00
+    nclatgps.vocabulary= "https://vocab.nerc.ac.uk/collection/OG1/current/LAT_GPS/"
+    # close netcdf file
+    src.close()
 
 logging.basicConfig(filename = 'process_delayed.log', level='INFO')
 
@@ -47,6 +114,7 @@ outname = seaexplorer.raw_to_timeseries(indir=rawncmergedir,
                                         profile_min_time=100, 
                                         fnamesuffix=fnamesuffix,
 					deadreckon=True)
+addGPS(inname=outname)
 # make profile netcdf files for ioos gdac
 ncprocess.extract_timeseries_profiles(inname=outname, 
                                       outdir=l0prfdir, 
